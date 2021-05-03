c75438712bb77e8cd5fb47db082b3f13e799248c
Jay <nhanlnt@hotmail.com>
2021-05-03 16:57:10 +0700

    working on Register

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import RouterOutlet from "./routes/RouterOutlet";
+import { Col } from "antd";
 
 import "./styles/rwd.css";
 import "./styles/App.css";
+import "antd/dist/antd.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
+      <Col xs={24} sm={24} md={24} lg={20} className="container">
         <RouterOutlet />
-      </div>
+      </Col>
     </div>
   );
 }
--- client/src/containers/Register.js
+++ client/src/containers/Register.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { Row, Col, Divider } from "antd";
 import { withRouter } from "react-router";
+
+import "../styles/Register.css";
 
 const Register = () => {
   return (
     <div className="register">
-      
+      <Row className="register-header">
+        <Col xs={24} sm={24} md={4} lg={4} className="header-logo">
+          <h1>.todolist</h1>
+        </Col>
+        <Col xs={24} sm={24} md={20} lg={20} className="header-content">
+          <h3>
+            Already a member? <button>Sign in</button>
+          </h3>
+        </Col>
+      </Row>
+      <Divider style={{ marginTop: 0, backgroundColor: "#e3e7f1" }} />
+      <Row className="register-form">
+        <Col lg={12} className="register-title">
+          <h2>.todolist</h2>
+          <h2>Sign up</h2>
+        </Col>
+        <Col lg={12}>col-12</Col>
+      </Row>
     </div>
   );
 };
